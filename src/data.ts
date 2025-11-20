86430a83f7ec99d16df57d7f5b2c5598ed92b585
Rafael Gutkowski <nounderline@libred.org>
2025-11-20 18:54:36 +0100

    add two unicorns

--- src/data.ts
+++ src/data.ts
@@ -1,9 +1,9 @@
 import BaliRiceFieldOutlineJpg from "../assets/BaliRiceField-outline.jpg"
 import BangkokArabicaOutlineJpg from "../assets/BangkokArabica-outline.jpg"
 import NZMountainsOutlineJpg from "../assets/NZMountains-outline.jpg"
-import SofaTrioOutlineJpg from "../assets/SofaTrio-outline.jpg"
 import SleepingUnicornColorPng from "../assets/SleepingUnicorn-color.png"
-import TwoUnicornsPlayingPng from "../assets/TwoUnicornsPlaying.png"
+import SofaTrioOutlineJpg from "../assets/SofaTrio-outline.jpg"
+import TwoUnicornsPlayingPng from "../assets/TwoUnicornsPlaying-color.png"
 
 export const Drawings = {
   "BaliRiceField": {
