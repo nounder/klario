44a09f5800bc0b2817cb77f5cae59a4f126910ba
Rafael Gutkowski <nounderline@libred.org>
2025-11-20 18:50:16 +0100

    new assets

--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,8 @@ import BaliRiceFieldOutlineJpg from "../assets/BaliRiceField-outline.jpg"
 import BangkokArabicaOutlineJpg from "../assets/BangkokArabica-outline.jpg"
 import NZMountainsOutlineJpg from "../assets/NZMountains-outline.jpg"
 import SofaTrioOutlineJpg from "../assets/SofaTrio-outline.jpg"
+import SleepingUnicornColorPng from "../assets/SleepingUnicorn-color.png"
+import TwoUnicornsPlayingPng from "../assets/TwoUnicornsPlaying.png"
 
 export const Drawings = {
   "BaliRiceField": {
@@ -15,5 +17,11 @@ export const Drawings = {
   },
   "SofaTrio": {
     imageUrl: SofaTrioOutlineJpg,
+  },
+  "SleepingUnicorn": {
+    imageUrl: SleepingUnicornColorPng,
+  },
+  "TwoUnicornsPlaying": {
+    imageUrl: TwoUnicornsPlayingPng,
   },
 }
