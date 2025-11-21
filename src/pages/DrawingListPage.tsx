1ebcb3b10b2688836209188cc64fd426238b93bc
Rafael Gutkowski <nounderline@libred.org>
2025-11-21 20:06:33 +0100

    add vertical scrolling

--- src/pages/DrawingListPage.tsx
+++ src/pages/DrawingListPage.tsx
@@ -5,7 +5,7 @@ import Headline from "../Headline.tsx"
 export function DrawingListPage() {
   return (
     <>
-      <div class="flex-1 flex flex-col min-w-0 h-full m-4">
+      <div class="flex-1 flex flex-col min-w-0 m-4">
         <div class="mx-auto mt-8 mb-8 w-full max-w-3xl">
           <Headline />
         </div>
